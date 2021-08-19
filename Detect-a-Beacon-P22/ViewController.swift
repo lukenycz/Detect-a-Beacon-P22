7d7cb7b63a687ca2ffb82bb2a9e5ab84e21cbafb
Łukasz Nycz <lnycz96@gmail.com>
2021-08-19 16:02:24 +0200

    Requesting location: Core Location

--- Detect-a-Beacon-P22/ViewController.swift
+++ Detect-a-Beacon-P22/ViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
     }
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    
+                }
+            }
+        }
+    }
 
 }
 
